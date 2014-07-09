608891898d025430a6863c4411ca9781b3380295
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-07-09 17:20:23 +0300

    done

--- app.js
+++ app.js
@@ -8,17 +8,23 @@ var logger = require('./source/utils/logger');
 var port = process.env.PORT || 3010;
 var proxy = httpProxy.createProxyServer();
 
-var server = http.createServer(function (req, res) {
-	var query = url.parse(req.url, true).query;
-	var accessToken = query.access_token;
+var parseAccessToken = function (req) {
+	var request = url.parse(req.url, true).query;
+	var referer = url.parse(req.headers.referer || '', true).query;
 
-	logger.info('request: ' + req.url + ' accessToken: ' + accessToken);
+	return request.access_token || referer.access_token;
+};
+
+var server = http.createServer(function (req, res) {
+	var accessToken = parseAccessToken(req);
+
+	logger.info('request: ' + req.url + ' accessToken: ' + accessToken + ' referer: ' + req.headers.referer);
 
 	if (!accessToken || accessToken !== config.accessToken) {
 		return res.end();
 	}
 
-	proxy.web(req, res, {target: config.target });
+	proxy.web(req, res, {target: config.target});
 });
 
 server.listen(port, function () {
--- config/production.config.js
+++ config/production.config.js
@@ -1,7 +1,11 @@
 var config = {
 	logentries: {
 		token: null
-	}
+	},
+
+	target: 'http://localhost:9200',
+
+	accessToken: process.env.PROXY_ACCESS_TOKEN
 };
 
 module.exports = config;
